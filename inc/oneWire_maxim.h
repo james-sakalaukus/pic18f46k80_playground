46b64df58f1882a3a6bd6733068a1ce25c858308
james <james@sakalaukus.com>
2015-05-25 16:42:53 -0400

    Yep

--- inc/oneWire_maxim.h
+++ inc/oneWire_maxim.h
@@ -8,37 +8,63 @@
 #ifndef ONEWIRE_MAXIM_H_
 #define ONEWIRE_MAXIM_H_
 
+#include <stdint.h>
 #include <stdbool.h>
 
 
-// definitions
-//#define FALSE 0
-//#define TRUE  1
-
 // method declarations
-int  OWFirst();
-int  OWNext();
-int  OWVerify();
-void OWTargetSetup(unsigned char family_code);
-void OWFamilySkipSetup();
-int  OWReset();
-void OWWriteByte(unsigned char byte_value);
-void OWWriteBit(unsigned char bit_value);
-unsigned char OWReadBit();
-int  OWSearch();
-unsigned char docrc8(unsigned char value);
+uint8_t OWFirst();
+uint8_t OWNext();
+uint8_t OWVerify();
+uint8_t OWReset();
+void OWWriteByte(uint8_t byte_value);
+void OWWriteBit(uint8_t bit_value);
+uint8_t OWReadBit();
+uint8_t OWSearch();
+uint8_t docrc8(uint8_t value);
 
 // global search state
-unsigned char ROM_NO[8];
-int LastDiscrepancy;
-int LastFamilyDiscrepancy;
-int LastDeviceFlag;
-unsigned char crc8;
+uint8_t ROM_NO[8];
+uint8_t LastDiscrepancy;
+uint8_t LastFamilyDiscrepancy;
+uint8_t LastDeviceFlag;
+uint8_t crc8;
 
 // Pause for exactly 'tick' number of ticks = 0.25us
-void tickDelay(int tick); // Implementation is platform specific
+void tickDelay(uint16_t tick); // Implementation is platform specific
 
 // 'tick' values
-int A,B,C,D,E,F,G,H,I,J;
+uint16_t A,B,C,D,E,F,G,H,I,J;
+
+// Standard Speed
+const uint16_t DELAY_A = 6 * 4;
+const uint16_t DELAY_B = 64 * 4;
+const uint16_t DELAY_C = 60 * 4;
+const uint16_t DELAY_D = 10 * 4;
+const uint16_t DELAY_E = 9 * 4;
+const uint16_t DELAY_F = 55 * 4;
+const uint16_t DELAY_G = 0;
+const uint16_t DELAY_H = 480 * 4;
+const uint16_t DELAY_I = 70 * 4;
+const uint16_t DELAY_J = 410 * 4;
+
+// TEST BUILD
+static uint8_t dscrc_table[] = {
+        0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,
+      157,195, 33,127,252,162, 64, 30, 95,  1,227,189, 62, 96,130,220,
+       35,125,159,193, 66, 28,254,160,225,191, 93,  3,128,222, 60, 98,
+      190,224,  2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
+       70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89,  7,
+      219,133,103, 57,186,228,  6, 88, 25, 71,165,251,120, 38,196,154,
+      101, 59,217,135,  4, 90,184,230,167,249, 27, 69,198,152,122, 36,
+      248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91,  5,231,185,
+      140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
+       17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
+      175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
+       50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
+      202,148,118, 40,171,245, 23, 73,  8, 86,180,234,105, 55,213,139,
+       87,  9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
+      233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
+      116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53};
 
 #endif /* ONEWIRE_MAXIM_H_ */
